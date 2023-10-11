edebf46a231a6abdae343b47985b7021f0eaf03d
Galabeam <68124962+Galabeam@users.noreply.github.com>
2023-10-11 19:06:28 -0400

    the most stressful update of my life

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Nothing yet!
+window.addEventListener("DOMContentLoaded", (event) => {
+    $ (function() {
+        $ ("#naviframe").load("navigation.html");
+    })
+
+    window.addEventListener('load', function() {
+        document.getElementById("nav" + document.body.id).style.color = "white"
+    })
+})
