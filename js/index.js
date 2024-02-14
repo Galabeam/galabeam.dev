293aa7b5ac09575f2e4ddf06ece5561f650997ac
Galabeam <galabeam76@gmail.com>
2024-02-14 12:03:25 -0500

    un-gh

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     $ (function() {
         $ ("#naviframe").load("/html/navigation.html");
-        $ ("head").load("/html/gh.html");
+        //$ ("head").load("/html/gh.html");
     })
 
     document.addEventListener('load', function() {
